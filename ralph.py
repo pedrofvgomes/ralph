a68ee3c0a6bf71bea209d6f231f07579f17eeae1
Pedro Gomes <up202108825@up.pt>
2023-03-05 01:31:03 +0000

    module imported, first black window working

--- ralph.py
+++ ralph.py
@@ -1,2 +1,13 @@
 import pygame
 
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((640,480))
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: pygame.quit()
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
+main()
