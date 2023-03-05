f3ffd5196bc23c2644c6d072b5ed0d24a3cd3910
Pedro Gomes <up202108825@up.pt>
2023-03-05 22:43:31 +0000

    bug fixing lateral jumps

--- ralph.py
+++ ralph.py
@@ -31,49 +31,94 @@ class Felix:
         self.x = 170
         self.y = 384
         self.status = 'standing'
+
     def left(self):
         # start moving
         if self.status == 'standing' and self.x >= 170:
             self.status = 'jumping left'
+
     def right(self):
         if self.status == 'standing' and self.x <= 409:
             self.status = 'jumping right'
+
     def up(self):
-        if self.y < 2 : self.y += 1
+        if self.status == 'standing' and self.y < 409:
+            self.status = 'jumping up'
+
     def down(self):
-        if self.y > 0 : self.y -= 1
+        if self.status == 'standing' and self.y >= 141:
+            self.status = 'jumping down'
+
     def draw(self, screen):
         if self.status == 'jumping left':
             self.x-=1
-            if (self.x > 170 and self.x < 203.5) or (self.x > 382 and self.x < 415.5):
+            # jumps 4-3 and 1-0 1st and 2nd floor
+            if (self.x > 170 and self.x < 203.5) or (self.x > 382 and self.x < 415.5) and self.y > 200:
                 self.y+=0.5
-            if (self.x > 203.5 and self.x < 237) or (self.x > 415.5 and self.x < 449):
+            if (self.x > 203.5 and self.x < 237) or (self.x > 415.5 and self.x < 449) and self.y > 200:
                 self.y-=0.5
-            if self.x in [245,249,253,257,261,265,269,273,277,281,285,289,293,297,301,305]:
+            
+            # jumps 3-2 and 2-1 1st floor
+            if self.x in [245,249,253,257,261,265,269,273,277,281,285,289,293,297,301,305] and self.y > 350:
                 self.y-=1
-            if self.x in [313,317,321,325,329,333,337,341,345,349,353,357,361,365,369,373]:
+            if self.x in [313,317,321,325,329,333,337,341,345,349,353,357,361,365,369,373] and self.y > 350:
                 self.y+=1
+
+            # jumps 3-2 and 2-1 1st floor
+            if self.x in [245,249,253,257,261,265,269,273,277,281,285,289,293,297,301,305] and self.y > 200 and self.y < 300:
+                self.y-=0.5
+            if self.x in [313,317,321,325,329,333,337,341,345,349,353,357,361,365,369,373] and self.y > 200 and self.y < 300:
+                self.y+=0.5
+
+            # every 3rd floor jump
+            if (self.x > 170 and self.x < 203.5) or (self.x > 237 and self.x < 273) or (self.x > 309 and self.x < 345) or (self.x > 382 and self.x < 415.5):
+                if self.y < 200: self.y+=0.5
+            if (self.x > 203.5 and self.x < 237) or (self.x > 273 and self.x < 309) or (self.x > 345 and self.x < 382) or (self.x > 415.5 and self.x < 449):
+                if self.y < 200: self.y-=0.5
+
         if self.status == 'jumping right':
             self.x+=1
-            if (self.x > 170 and self.x < 203.5) or (self.x > 382 and self.x < 415.5):
+            # jumps 0-1 and 3-4 1st and 2nd floor
+            if (self.x > 170 and self.x < 203.5) or (self.x > 382 and self.x < 415.5) and self.y > 200:
                 self.y-=0.5
-            if (self.x > 203.5 and self.x < 237) or (self.x > 415.5 and self.x < 449):
+            if (self.x > 203.5 and self.x < 237) or (self.x > 415.5 and self.x < 449) and self.y > 200:
                 self.y+=0.5
-            if self.x in [245,249,253,257,261,265,269,273,277,281,285,289,293,297,301,305]:
+
+            # jumps 1-2 and 2-3 1st floor
+            if self.x in [245,249,253,257,261,265,269,273,277,281,285,289,293,297,301,305] and self.y > 350:
                 self.y+=1
-            if self.x in [313,317,321,325,329,333,337,341,345,349,353,357,361,365,369,373]:
+            if self.x in [313,317,321,325,329,333,337,341,345,349,353,357,361,365,369,373] and self.y > 350:
                 self.y-=1
+
+            # jumps 3-2 and 2-1 1st floor
+            if self.x in [245,249,253,257,261,265,269,273,277,281,285,289,293,297,301,305] and self.y > 200 and self.y < 300:
+                self.y+=0.5
+            if self.x in [313,317,321,325,329,333,337,341,345,349,353,357,361,365,369,373] and self.y > 200 and self.y < 300:
+                self.y-=0.5
+            
+            # every 3rd floor jump
+            if (self.x > 170 and self.x < 203.5) or (self.x > 237 and self.x < 273) or (self.x > 309 and self.x < 345) or (self.x > 382 and self.x < 415.5):
+                if self.y < 200: self.y-=0.5
+            if (self.x > 203.5 and self.x < 237) or (self.x > 273 and self.x < 309) or (self.x > 345 and self.x < 382) or (self.x > 415.5 and self.x < 449):
+                if self.y < 200: self.y+=0.5
+        
+        if self.status == 'jumping up':
+            self.y -= 1
+        
+        if self.status == 'jumping down':
+            self.y += 1
 
         # standing positions
         if (self.x, self.y) in [(170,384), (170,263), (170,141), (237,384), (237,263), (237,141), (309,400), (309,271), (309,141), (382,384), (382,263), (382,141), (449,384), (449,263), (449,141)]:
             self.status = 'standing'
-
 
         if self.status == 'standing':
             icon = pygame.image.load('images/felix_standing.png')
         if self.status == 'jumping left':
             icon = pygame.image.load('images/felix_jumping_left.png')
         if self.status == 'jumping right':
+            icon = pygame.image.load('images/felix_jumping_right.png')
+        if self.status in ['jumping down', 'jumping up']:
             icon = pygame.image.load('images/felix_jumping_right.png')
     
         screen.blit(icon, (self.x, self.y))
